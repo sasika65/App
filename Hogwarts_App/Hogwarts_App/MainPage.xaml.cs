0a404314160446cb46d63c8559b4080809f6aaef
Sasa Krajnc <kra40207873@CHIC.local>
2018-04-18 12:03:48 +0100

    Add project files.

---
+++ Hogwarts_App/Hogwarts_App/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Hogwarts_App
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new RunPage());
+        }
+        private void Button2_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Register_Page());
+        }
+    }
+}
---
+++ Hogwarts_App/Hogwarts_App/RunPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Org.W3c.Dom;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hogwarts_App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RunPage : TabbedPage
+    {
+        private Timer timer = new Timer();
+        int time;
+        
+
+        public RunPage()
+        {
+            InitializeComponent();
+            timer = new Timer();
+        }
+        private void Run(object sender, EventArgs e)
+        {
+            timer.Interval = 1;
+            timer.Elapsed += Timer_count;
+            timer.Enabled = true;
+        }
+        public void Timer_count(object sender, EventArgs e)
+        {
+            time++;
+            lblNumbers.Text = time.ToString();
+        }
+        private void Stop(object sender, EventArgs e)
+        {
+            timer.Enabled = false;
+        }
+    }
+}
+
