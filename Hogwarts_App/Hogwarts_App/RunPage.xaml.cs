8c7e39d2159863b7fd4e243cf6922125d0a5e62f
sasa krajnc <sasa.krajnc@hotmail.com>
2018-04-25 11:59:33 +0100

    Timer now works

--- Hogwarts_App/Hogwarts_App/RunPage.xaml.cs
+++ Hogwarts_App/Hogwarts_App/RunPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Android.Content;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,35 +8,59 @@ using System.Timers;
 using Org.W3c.Dom;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Android.App;
+using Android.Widget;
+using System.Threading;
+using Java.Lang;
 
 namespace Hogwarts_App
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RunPage : TabbedPage
     {
-        private Timer timer = new Timer();
+        private System.Timers.Timer timer;
         int time;
-        
+        Label lblForTheTime;
+        int min = 0;
+        int seconds = 0;
+        int miliseconds = 0;
 
         public RunPage()
         {
             InitializeComponent();
-            timer = new Timer();
+            timer = new System.Timers.Timer();
+            timer.AutoReset = true;
+            lblForTheTime = this.FindByName<Label>("number_label");
         }
         private void Run(object sender, EventArgs e)
         {
-            timer.Interval = 1;
             timer.Elapsed += Timer_count;
             timer.Enabled = true;
+            timer.Start();
+            timer.Interval = 1;
         }
         public void Timer_count(object sender, EventArgs e)
-        {
-            time++;
-            lblNumbers.Text = time.ToString();
+        {       
+            miliseconds++;
+            if (miliseconds >= 1000)
+            {
+                seconds++;
+                miliseconds = 0;
+            }
+            if (seconds >= 60)
+            {
+                min++;
+                seconds = 0;
+            }
+            //Connects to label
+            Device.BeginInvokeOnMainThread(() => { lblForTheTime.Text = min.ToString() + ":" + seconds.ToString() + ":" + miliseconds.ToString(); });
         }
+
+        
         private void Stop(object sender, EventArgs e)
         {
-            timer.Enabled = false;
+            timer.Stop();
+            
         }
     }
 }
